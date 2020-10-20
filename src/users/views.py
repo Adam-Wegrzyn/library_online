c03f5f6d3d8d2623ac6154302d522b37ba247751
Adam-Wegrzyn <64739433+Adam-Wegrzyn@users.noreply.github.com>
2020-10-20 13:35:59 +0200

    Update views.py

--- src/users/views.py
+++ src/users/views.py
@@ -3,16 +3,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     form = UserCreationForm
-
+    error = ''
     if request.method == 'POST':
         form = form(request.POST)
         if form.is_valid():
             form.save()
-        return redirect(reverse('import_book'))
-    else:
-        form = UserCreationForm()
-
+            return redirect(reverse('login'))
+        else:
+            error='Username/password or password confirmation is incorrect!'
+            form = UserCreationForm()
+            print(form.errors)
+            print(error)
+    print(form.errors)
     context = {
-        'form': form
+        'form': form,
+        'error': error
     }
     return render(request, 'register/register.html', context)
